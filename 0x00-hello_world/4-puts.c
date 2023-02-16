08ff7750ee5e14942c38b8d929cab1086d2b6c75
nornormal <niyramagdy@gmail.com>
2023-02-16 12:07:36 -0800

    put&betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This will print "Programming is like building a multilingual puzzle
+ * Return: This returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle)
+	Return (0)
+}
