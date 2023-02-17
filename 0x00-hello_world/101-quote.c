240d1b4fff1127e489b89b6432c551d3498ef64c
nornormal <niyramagdy@gmail.com>
2023-02-16 17:36:49 -0800

    return1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	char char = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
+	putchar(char);
+	return 1;
+}
