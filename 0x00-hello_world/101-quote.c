0a84d5627a569e8d52d71deaf55317581d8f1845
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-02-18 17:00:11 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\,"
+	char[]=("and that piece of art is useful\" - Dora Korpar, 2015-10-19\,");
 	putchar=(2, "char", 59);
 	return (1);
 }
