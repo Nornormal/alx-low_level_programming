71696126a2b1d6a1213fd75f25772dc3324672e7
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-02-20 02:22:22 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +8,6 @@
  */
 int main(void)
 {
-	char[100]=("\and that piece of art is useful\" - Dora Korpar, 2015-10-19,");
-	putchar=(2, str, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,", 59);
 	return (1);
 }
