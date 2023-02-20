864288a0b2bef29903ae31a967a08d13ac9b3338
nornormal <niyramagdy@gmail.com>
2023-02-20 10:35:47 -0800

    forloop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	retun(0);
+}
