3c0536189d08de16316bf0107d5b219a422cd9c3
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-02-20 04:46:07 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  *
- * main - deciding if a last digit of a number is greater than 5, 0 or less than 6 and not 0
+ * main - deciding if a number is greater than 5, 0 or less than 6 and not 0
  *
  * Return : Always 0 success
  */
