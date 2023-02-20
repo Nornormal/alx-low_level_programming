4c02c2bac2d08ec23ce51cd61c43e934c8ee91b0
nornormal <niyramagdy@gmail.com>
2023-02-20 12:15:42 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	char f;
+	int c;
+
+	f = 'a';
+	while (f <= 'z')
+	{
+		putchar('f');
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	char e, f;
+	int c, m;
+
+	e = 'a';
+	f = 'A';
+	while (e <= 'z')
+	{
+		putchar('e');
+		c = c + 1;
+	}
+	while (f <= 'Z')
+	{
+		putchar('f');
+		m = m + 1;
+	}
+	putchar('\n');
+	return (0);
+}
