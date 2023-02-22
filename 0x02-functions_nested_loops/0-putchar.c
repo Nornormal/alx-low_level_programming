e3ae03d2246f3d99c0a5c6a5ad304527f302dcf9
nornormal <niyramagdy@gmail.com>
2023-02-22 12:57:05 -0800

    function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 95, p = 112, u = 117, t = 116;
+	int c = 99, h = 104, a = 97, r = 114;
+
+	_putchar(i);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+
+	return (0);
+}
