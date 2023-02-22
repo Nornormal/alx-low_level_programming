c654822652ccb65b6badba2b3a3833068d456f9b
nornormal <niyramagdy@gmail.com>
2023-02-22 15:10:28 -0800

    function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check for lowercase character
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
