d8bdf454c4f415bb1968468a6887b5d9b26d3ed1
nornormal <niyramagdy@gmail.com>
2023-02-22 15:44:19 -0800

    function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check for alphabetic character
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (g >= 65 && g <= 90 || l >= 97 && l <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar(\n');
+}
