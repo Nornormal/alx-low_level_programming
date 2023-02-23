65b2c77f9cc742ba4963d8b18b8a6e38401486ac
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-02-23 02:40:09 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,17 +2,19 @@
 
 /**
  * print_sign - determine if a number
+ * greater or equal or smaller than zero
+ * @n: The input number
  *
  * Return: 1 for positive, 0 for zero and -1 for negative
  */
 int print_sign(int n)
 {
-	if (n >= 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0);
+	else if (n = 0);
 	{
 		_putchar(48);
 		return (0);
