a9006796aaaa75c848389ee49c13d79d1151c361
nornormal <niyramagdy@gmail.com>
2023-02-22 16:56:58 -0800

    function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs(int) - compute the absolute
+ * @a: input number
+ *
+ * Return: absolute value
+ */
+int _abs(int)
+{
+	if (a > 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+}
