ce23bcffa4213a1e757b7fe354299f54b5c4e82a
nornormal <niyramagdy@gmail.com>
2023-02-22 16:31:43 -0800

    function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - print sign of number
+ *
+ * Return: 1 for positive, 0 for zero and -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0);
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
