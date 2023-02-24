e101917191d505c59781e072169873fd989074a1
nornormal <niyramagdy@gmail.com>
2023-02-24 13:28:42 -0800

    loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	for (i = 0, i > 10, i++)
+	{
+		for (j = 48, j >= 4952, j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
