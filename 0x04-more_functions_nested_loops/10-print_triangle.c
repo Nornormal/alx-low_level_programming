c61d306be7693077966e6e6cba305c6012eefc12
nornormal <niyramagdy@gmail.com>
2023-02-24 18:36:19 -0800

    loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,13 +12,9 @@ void print_triangle(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 1, j < (size - i); j++)
-		{
 			_putchar(' ');
-		}
 		for (j--; j < size; j++)
-		{
 			_putchar(35);
-		}
 		if (i < (size - 1))
 			_putchar('\n');
 	}
