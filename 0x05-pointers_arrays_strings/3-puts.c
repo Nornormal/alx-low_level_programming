655d7e7e42f2d4ab82362bde8f6721e25526843c
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-02-28 01:16:59 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,11 @@
 void _puts(char *str);
 {
 	int = c;
-	for (c = 0; c >= 0; c++)
+	for (c = 0; c > 0; c++)
 	{
 		if (str[c] == '\0')
 		{
 			_putchar('\n');
-			break;
 		}
 	}
 	_putchar('str[c]');
