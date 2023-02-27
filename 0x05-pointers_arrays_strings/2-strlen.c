6ea450fc4f2e08f0233f1f45068a8b9f24b13252
nornormal <niyramagdy@gmail.com>
2023-02-27 15:02:49 -0800

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen -  a function that returns the length of a string.
- * @s: char
+ * @s: string
  * Return: returns the length of a string.
  */
 int _strlen(char *s)
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string.
+ * @str: string
+ * Reeturn - no return
+ */
+void _puts(char *str);
+{
+	int = *str;
+	for (*str = 0; *str < n; *str++)
+	{
+		_putchar('*str');
+	}
+	_putchar('\n');
+}
