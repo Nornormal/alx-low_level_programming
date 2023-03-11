b3b941353482fc6db8cdf923f6ae679f6cc437b2
nornormal <niyramagdy@gmail.com>
2023-03-10 18:43:25 -0800

    argument

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the minimum number of coins,
+ * to make change for an amount of money.
+ * @argc: number of command line arguments.
+ * @argv: the program command line arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: the program command line arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	return(0);
+}
