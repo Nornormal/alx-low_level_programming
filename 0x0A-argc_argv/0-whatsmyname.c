5fd4657b3c62611bc88c5cfd3f30874777a1ab6b
nornormal <niyramagdy@gmail.com>
2023-03-10 16:27:00 -0800

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - aprogram that prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: the program command line arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
