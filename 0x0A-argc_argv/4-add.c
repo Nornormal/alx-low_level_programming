b72f32ac248c0ce0550179fc7d1faa5c6a4c4b5d
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-03-16 01:42:38 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,19 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i, add = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
+		if (argv[i] >= '0' && argv[i] < '2000')
 			{
-				printf("Error\n");
-				return (1);
+				add = add + atoi(argv[i]);
 			}
+		else
+		{
+			printf("Error\n");
+			return (1);
 		}
-		add = add + atoi(argv[i]);
 	}
 	printf("%d\n", add);
 	return (0);
