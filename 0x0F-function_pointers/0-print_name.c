200eb2abbe3b086a0f4a8b7e81c80f2db1ac74da
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-03-23 02:11:54 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /*
- * print_name - function that prints a name
+ * print_name - function that prints a name.
  * @name: name
  * @f: a pointer to a function
  * Return - no return
