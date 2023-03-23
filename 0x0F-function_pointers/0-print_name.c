05a833c3e8c2a45e4f6d6018e63621f4159534fe
nornormal <niyramagdy@gmail.com>
2023-03-22 17:08:08 -0700

    pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/*
+ * print_name - function that prints a name
+ * @name: name
+ * @f: a pointer to a function
+ * Return - no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	_putchar("*name\n");
+}
