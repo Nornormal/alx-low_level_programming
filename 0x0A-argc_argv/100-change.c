0e2dcaa987bdfaef3b90b203c94ba94f693aef1a
nornormal <niyramagdy@gmail.com>
2023-03-30 20:00:18 -0700

    arguments

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,6 +10,29 @@
  */
 int main(int argc, char *argv[])
 {
+	char *dup;
+	int len;
+	list_t *new;
 
-	return (0);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
