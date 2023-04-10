3f2727c5288d1b0a79b5d14044b462cc4e2df3cf
nornormal <niyramagdy@gmail.com>
2023-04-10 16:12:09 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum = 0;
-	int i, j, len = 0;
+	int i, j = 1, len = 0;
 
 	if (b == NULL)
 		return (0);
@@ -19,13 +19,16 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] != '\0'; i++)
 		len += b[i];
 
-	for (i = (len - 1); i >= 0; i--)
+	for (i--; i >= 0; i--, j *= 2)
 	{
-		for (j = 0; j <= 7; j++)
+		if (b[i] != '0' && b[i] != '1')
 		{
-			if (b[i] != '0' && b[i] != '1')
-				return (0);
-			sum += i << j;
+			return (0);
+		}
+		
+		if (b[i] & 1)
+		{
+			sum += j;
 		}
 	}
 	return (sum);
