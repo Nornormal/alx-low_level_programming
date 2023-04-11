81f6e3ba772518f0c01a850102c007c0958aea77
nornormal <niyramagdy@gmail.com>
2023-04-10 19:51:18 -0700

    bit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename is the name of the file
+ * @text_content is the NULL terminated string to add
+ * 
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fo;
+	int letterno;
+	int wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fo = open(filename, O_WRONLY | O_APPEND);
+
+	if (fo == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (letterno = 0; text_content[letterno]; letterno++)
+			;
+
+		wr = write(fo, text_content, letterno);
+
+		if (wr == -1)
+			return (-1);
+	}
+
+	close(fo);
+
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,108 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char *creat_buff(char *file);
+void close_file(int fo);
+
+/**
+ * creat_buff - allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: a pointer to the allocated buffer.
+ */
+char *creat_buff(char *file)
+{
+	char *buff;
+
+	buff = malloc(sizeof(char) * 1024);
+
+	if (buff == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buff);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int d;
+
+	d = close(fd);
+
+	if (d == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copy the contents of a file to another file.
+ * @argc: The number of arguments.
+ * @argv: array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buff;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buff = creat_buff(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buff);
+			exit(98);
+		}
+
+		w = write(to, buff, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buff);
+			exit(99);
+		}
+
+		r = read(from, buff, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buff);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
