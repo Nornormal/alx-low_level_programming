a596594f967ec9489497e9685318ca1a3d053967
nornormal <niyramagdy@gmail.com>
2023-04-10 19:54:17 -0700

    bit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,9 +7,8 @@
 
 /**
  * append_text_to_file - appends text at the end of a file.
- * @filename is the name of the file
- * @text_content is the NULL terminated string to add
- * 
+ * @filename: is the name of the file
+ * @text_content: is the NULL terminated string to add
  * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
