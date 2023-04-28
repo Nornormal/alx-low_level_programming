d4a225c9040fb3fb4947b85d8fc0fb8db234035f
Nornormal <niyramagdy@gmail.com>
2023-04-28 15:44:09 -0700

    Bitwise

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - a function that checks the endianness.
+ * 
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int n = 1;
+	char *en = (char *)&n;
+
+	if (*en == 1)
+		return (1);
+
+	return (0);
+}
