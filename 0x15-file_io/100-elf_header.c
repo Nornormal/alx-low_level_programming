5cb5ad5a0e4fa5dec842245da56ad1763f0890b3
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-04-29 03:56:00 +0200

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -50,6 +50,79 @@ void prnt_adr(char *point)
 }
 
 /**
+ * prnt_osb - function that prints osabi
+ * @point: a pointer to osabi.
+ * Return: nothing.
+ */
+void prnt_osb(char *point)
+{
+	char osb = point[7];
+
+	printf("  OS/ABI:                            ");
+	if (osb == 0)
+		printf("UNIX - System V\n");
+	else if (osb == 2)
+		printf("UNIX - NetBSD\n");
+	else if (osb == 6)
+		printf("UNIX - Solaris\n");
+	else
+		printf("<unknown: %x>\n", osb);
+
+	printf("  ABI Version:                       %d\n", point[8]);
+}
+
+/**
+ * prnt_mag - functprints magic info.
+ * @point: pointer to magic.
+ * Return: nothing.
+ */
+void prnt_mag(char *point)
+{
+	int byt;
+
+	printf("  Magic:  ");
+	for (byt = 0; byt < 16; byt++)
+		printf(" %02x", ptr[byt]);
+
+	printf("\n");
+
+}
+
+/**
+ * prnt_ver - function that prints version
+ * @point: pointer to version.
+ * Return: nothing.
+ */
+void prnt_ver(char *point)
+{
+	int ver = point[6];
+
+	printf("  Version:                           %d", ver);
+
+	if (ver == EV_CURRENT)
+		printf(" (current)");
+
+	printf("\n");
+}
+
+/**
+ * prnt_dt - function that prints data
+ * @point: pointer to data.
+ * Return: nothing.
+ */
+void prnt_dt(char *point)
+{
+	char d = point[5];
+
+	printf("  Data:                              2's complement");
+	if (d == 1)
+		printf(", little endian\n");
+
+	if (d == 2)
+		printf(", big endian\n");
+}
+
+/**
  * prnt_typ - function that print type
  * @point: a pointer to type
  * Return: nothing.
@@ -79,166 +152,91 @@ void prnt_typ(char *point)
 }
 
 /**
- * prnt_osb - function that prints osabi
- * @point: a pointer to osabi.
+ * chk_sstem - function that check the version system.
+ * @point: pointer to version system.
  * Return: nothing.
  */
-void prnt_osb(char *point)
+void chk_sstem(char *point)
 {
-	char osb = point[7];
+	char sstem = point[4] + '0';
 
-	printf("  OS/ABI:                            ");
-	if (osb == 0)
-		printf("UNIX - System V\n");
-	else if (osb == 2)
-		printf("UNIX - NetBSD\n");
-	else if (osb == 6)
-		printf("UNIX - Solaris\n");
-	else
-		printf("<unknown: %x>\n", osb);
-
-	printf("  ABI Version:                       %d\n", point[8]);
-}
-
-
-/**
- * prnt_ver - function that prints version
- * @point: pointer to version.
- * Return: nothing.
- */
-void prnt_ver(char *point)
-{
-	int ver = point[6];
-
-	printf("  Version:                           %d", ver);
-
-	if (ver == EV_CURRENT)
-		printf(" (current)");
-
-	printf("\n");
-}
-/**
- * prnt_dt - function that prints data
- * @point: pointer to data.
- * Return: nothing.
- */
-void prnt_dt(char *point)
-{
-	char d = point[5];
-
-	printf("  Data:                              2's complement");
-	if (d == 1)
-		printf(", little endian\n");
-
-	if (d == 2)
-		printf(", big endian\n");
-}
-/**
- * prnt_mag - functprints magic info.
- * @ptr: magic.
- * Return: no return.
- */
-void print_magic(char *ptr)
-{
-	int bytes;
-
-	printf("  Magic:  ");
-
-	for (bytes = 0; bytes < 16; bytes++)
-		printf(" %02x", ptr[bytes]);
-
-	printf("\n");
-
-}
-
-/**
- * check_sys - check the version system.
- * @ptr: magic.
- * Return: no return.
- */
-void check_sys(char *ptr)
-{
-	char sys = ptr[4] + '0';
-
-	if (sys == '0')
+	if (sstem == '0')
 		exit(98);
 
 	printf("ELF Header:\n");
-	print_magic(ptr);
+	prnt_mag(point);
 
-	if (sys == '1')
+	if (sstem == '1')
 		printf("  Class:                             ELF32\n");
 
 	if (sys == '2')
 		printf("  Class:                             ELF64\n");
 
-	print_data(ptr);
-	print_version(ptr);
-	print_osabi(ptr);
-	print_type(ptr);
-	print_addr(ptr);
+	prnt_dt(point);
+	prnt_vers(point);
+	prnt_osb(point);
+	prnt_typ(point);
+	prnt_adr(point);
 }
 
 /**
- * check_elf - check if it is an elf file.
- * @ptr: magic.
- * Return: 1 if it is an elf file. 0 if not.
+ * chk_elf - function that check elf file.
+ * @point: pointer to elf file.
+ * Return: 1 or 0.
  */
-int check_elf(char *ptr)
+int chk_elf(char *point)
 {
-	int addr = (int)ptr[0];
-	char E = ptr[1];
+	int adr = (int)point[0];
+	char E = point[1];
 	char L = ptr[2];
 	char F = ptr[3];
 
-	if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
+	if (adr == 127 && E == 'E' && L == 'L' && F == 'F')
 		return (1);
 
 	return (0);
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
+ * main - program that displays the information
+ * contained in the ELF header at the start of an ELF file.
+ * @ac: number in aarray arguments.
+ * @av: array of arguments.
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-	int fd, ret_read;
-	char ptr[27];
+	int fo, retn_red;
+	char point[27];
 
-	if (argc != 2)
+	if (ac != 2)
 	{
 		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
 		exit(98);
 	}
 
-	fd = open(argv[1], O_RDONLY);
+	fo = open(av[1], O_RDONLY);
 
-	if (fd < 0)
+	if (fo < 0)
 	{
 		dprintf(STDERR_FILENO, "Err: file can not be open\n");
 		exit(98);
 	}
+	lseek(fo, 0, SEEK_SET);
+	retn_red = read(fo, point, 27);
 
-	lseek(fd, 0, SEEK_SET);
-	ret_read = read(fd, ptr, 27);
-
-	if (ret_read == -1)
+	if (retn_red == -1)
 	{
 		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
 		exit(98);
 	}
-
-	if (!check_elf(ptr))
+	if (!chk_elf(point))
 	{
 		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
 		exit(98);
 	}
-
-	check_sys(ptr);
-	close(fd);
+	chk_sstem(point);
+	close(fo);
 
 	return (0);
 }
